2af8460f288e1b46a390971386f11f158dd21663
boostrap <160605450+rafey890@users.noreply.github.com>
2024-03-05 13:33:27 -0800

    first assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello World");
+console.log('My name is Syed Rafey-Ul-Hasan');
+console.log("I am a student of governor initiative program");
+console.log("my age is", 21);
+console.log("my phone no:03433009990");
---
+++ main.ts
@@ -0,0 +1,5 @@
+console.log("Hello World");
+console.log('My name is Syed Rafey-Ul-Hasan');
+console.log("I am a student of governor initiative program")
+console.log("my age is", 21);
+console.log("my phone no:03433009990");
